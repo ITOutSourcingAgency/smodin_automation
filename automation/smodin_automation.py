d09699c2fbf7deb5bca544d597384ce6d4ebb94c
yback1223 <yoyob1223@gmail.com>
2024-07-01 23:24:53 +0900

    로그인 오류 발생시 자동으로 재시작, 템플릿 파일 적용시 마지막 내용 오류 수정

--- automation/smodin_automation.py
+++ automation/smodin_automation.py
@@ -48,74 +48,83 @@ class SmodinAutomation:
 
 		except Exception as e:
 			self.settings.add_log("프로그램을 강제 종료 하셨거나 오류가 발생했습니다.", "red")
-			print(f"An error occurred during login: {e}")
 		finally:
 			self.settings.add_log("자동화 작업을 종료합니다.", "grey")
 			self.driver.quit()
 
 	def login(self, actions):
-		self.driver.get('https://app.smodin.io/ko/login')
-		try:
-			main_window = self.driver.current_window_handle
-			google_login_button = WebDriverWait(self.driver, 30).until(
-				EC.presence_of_element_located((By.XPATH, '/html/body/div/div[3]/div/button'))
-			)
-			google_login_button.click()
-			WebDriverWait(self.driver, 30).until(EC.new_window_is_opened([main_window]))
-			all_windows = self.driver.window_handles
+		max_retries = 5
+		retries = 0
+		
+		while retries < max_retries:
+			try:
+				self.driver.get('https://app.smodin.io/ko/login')
+				main_window = self.driver.current_window_handle
+				google_login_button = WebDriverWait(self.driver, 10).until(
+					EC.presence_of_element_located((By.XPATH, '/html/body/div/div[3]/div/button'))
+				)
+				google_login_button.click()
+				WebDriverWait(self.driver, 10).until(EC.new_window_is_opened([main_window]))
+				all_windows = self.driver.window_handles
 
-			for window in all_windows:
-				if window != main_window:
-					self.driver.switch_to.window(window)
-					break
-
-			id_input = WebDriverWait(self.driver, 30).until(
-				EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[2]/div/div/div[1]/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input'))
-			)
-			id_input.click()
-			clipboard.copy(self.settings.id.get())
-			
-			if platform.system() == 'Darwin':  
-				actions.key_down(Keys.COMMAND).send_keys('v').key_up(Keys.COMMAND).perform()
-			else:
-				actions.key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
-
-			id_submit_button = WebDriverWait(self.driver, 10).until(
-				EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[1]/div/div/button'))
-			)
-			id_submit_button.click()
-
-			pw_input = WebDriverWait(self.driver, 60).until(
-				EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[2]/div/div/div[1]/form/span/section[2]/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input'))
-			)
-			pw_input.click()
-			clipboard.copy(self.settings.pw.get())
-
-			if platform.system() == 'Darwin':  
-				actions.key_down(Keys.COMMAND).send_keys('v').key_up(Keys.COMMAND).perform()
-			else:
-				actions.key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
-
-			pw_submit_button = WebDriverWait(self.driver, 10).until(
-				EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[1]/div/div/button'))
-			)
-			pw_submit_button.click()
-			login_window_handles = self.driver.window_handles
-
-			if len(login_window_handles) > 1:
-				start_time = time.time()
-				max_wait_time = 300
-			
-				while time.time() - start_time < max_wait_time:
-					tmp_window_handles = self.driver.window_handles
-					if len(tmp_window_handles) == 1:
+				for window in all_windows:
+					if window != main_window:
+						self.driver.switch_to.window(window)
 						break
-					time.sleep(2)
 
-			self.driver.switch_to.window(main_window)
-			
-		except Exception as e:
-			print(f"An error occurred during the login process: {e}")
+				id_input = WebDriverWait(self.driver, timeout=10).until(
+					EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[2]/div/div/div[1]/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input'))
+				)
+				id_input.click()
+				clipboard.copy(self.settings.id.get())
+				
+				if platform.system() == 'Darwin':  
+					actions.key_down(Keys.COMMAND).send_keys('v').key_up(Keys.COMMAND).perform()
+				else:
+					actions.key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
+
+				id_submit_button = WebDriverWait(self.driver, 10).until(
+					EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[1]/div/div/button'))
+				)
+				id_submit_button.click()
+
+				pw_input = WebDriverWait(self.driver, 20).until(
+					EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[2]/div/div/div[1]/form/span/section[2]/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input'))
+				)
+				pw_input.click()
+				clipboard.copy(self.settings.pw.get())
+
+				if platform.system() == 'Darwin':  
+					actions.key_down(Keys.COMMAND).send_keys('v').key_up(Keys.COMMAND).perform()
+				else:
+					actions.key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
+
+				pw_submit_button = WebDriverWait(self.driver, 10).until(
+					EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[1]/div/div/button'))
+				)
+				pw_submit_button.click()
+				login_window_handles = self.driver.window_handles
+
+				if len(login_window_handles) > 1:
+					start_time = time.time()
+					max_wait_time = 300
+				
+					while time.time() - start_time < max_wait_time:
+						tmp_window_handles = self.driver.window_handles
+						if len(tmp_window_handles) == 1:
+							break
+						time.sleep(2)
+
+				self.driver.switch_to.window(main_window)
+				break  # 성공적으로 로그인하면 루프 종료
+				
+			except Exception as e:
+				self.settings.add_log(f"로그인 도중에 에러가 발생했습니다.", "red")
+				retries += 1
+				if retries < max_retries:
+					self.settings.add_log(f"로그인을 다시 시도합니다.")
+				else:
+					self.settings.add_log(f"로그인이 지속해서 실패하고 있습니다. 프로그램을 종료하고 다시 실행해주세요.", "blue")
 
 	def select_options_paid(self, actions, one_setting, one_file):
 		self.driver.get('https://app.smodin.io/ko/%E1%84%86%E1%85%AE%E1%84%85%E1%85%AD%E1%84%85%E1%85%A9%E1%84%92%E1%85%A1%E1%86%AB%E1%84%80%E1%85%AE%E1%86%A8%E1%84%8B%E1%85%A5%E1%84%85%E1%85%A9%E1%84%90%E1%85%A6%E1%86%A8%E1%84%89%E1%85%B3%E1%84%90%E1%85%B3%E1%84%8C%E1%85%A1%E1%84%83%E1%85%A9%E1%86%BC%E1%84%87%E1%85%A5%E1%86%AB%E1%84%8B%E1%85%A7%E1%86%A8')
@@ -320,12 +329,37 @@ class SmodinAutomation:
 			template_content = template_content[:next_inquote_idx] + chunk + '\n\n' + template_content[next_inquote_idx:]
 			search_start_idx = template_content.find('>\n', next_inquote_idx + len(chunk))
 
-		if remaining_chunks:
-			last_placeholder_idx = template_content.rfind('}>')
-			if last_placeholder_idx != -1:
-				template_content = template_content[:last_placeholder_idx+2] + '\n\n' + '\n\n'.join(remaining_chunks).strip() + template_content[last_placeholder_idx+2:]
-			else:
-				template_content += '\n\n' + '\n\n'.join(remaining_chunks).strip()
+			if remaining_chunks:
+				last_placeholder_idx = template_content.rfind('<인용구')
+				if last_placeholder_idx != -1:
+					# 마지막 <인용구> 태그의 끝 부분(>) 인덱스를 찾음
+					end_of_last_inquote_idx = template_content.find('>', last_placeholder_idx) + 1
+
+					# 파일 끝까지 탐색하여 공백이나 개행이 아닌 부분까지 인덱스를 증가시킴
+					next_line_start_idx = end_of_last_inquote_idx
+					while next_line_start_idx < len(template_content) and template_content[next_line_start_idx] in ['\n', ' ']:
+						next_line_start_idx += 1
+
+					# <인용구> 다음 문장의 개행을 찾음
+					next_line_end_idx = template_content.find('\n', next_line_start_idx)
+					if next_line_end_idx == -1:
+						next_line_end_idx = len(template_content)  # 파일의 끝까지 문장이 있는 경우
+
+					# 개행이 있다면 그 다음 위치에 삽입 인덱스를 설정, 개행이 없다면 end_of_last_inquote_idx를 삽입 인덱스로 설정
+					if next_line_start_idx < next_line_end_idx:
+						insertion_idx = template_content.find('\n', next_line_end_idx) + 1
+						if insertion_idx == 0:  # 개행을 찾지 못한 경우
+							insertion_idx = len(template_content)
+					else:
+						insertion_idx = end_of_last_inquote_idx
+
+					# remaining_chunks를 삽입 인덱스 위치에 추가
+					template_content = template_content[:insertion_idx] + '\n\n' + '\n\n'.join(remaining_chunks).strip() + template_content[insertion_idx:]
+				else:
+					# <인용구> 태그가 없으면 remaining_chunks를 템플릿 끝에 추가
+					template_content += '\n\n' + '\n\n'.join(remaining_chunks).strip()
+
+
 
 		tmp_index = ''
 		if index == 0:
--- etc/__init__.py
+++ etc/__init__.py
@@ -1,3 +1,4 @@
 DEV = False
+# DEV = True
 AUTH_NAMESPACE = '스모딘변환프로그램'
 AUTH_ID = '곽진천1'
